ffbfeeba39adaa8252129f3a981fa8df39c73afe
emreakdik <yemre.akdik@gmail.com>
2023-10-22 16:04:10 +0300

    fix update

--- install.sh
+++ install.sh
@@ -1,55 +1,50 @@
 
 install_dir="$HOME/goinfre/apps"
 
+# check if there .src 
+if [ -d ~/.src ]; then
+    rm -rf ~/.src
+else
+    mkdir -p ~/.src
+fi
+
 mkdir -p ~/.src
 cp src/* ~/.src
 cp -r .git ~/.src
 
 # check if there is alias 
 if [ -f ~/.zshrc ]; then
-    if grep -q "alias download='bash ~/.src/download.sh'" ~/.zshrc; then
-        echo "alias download already exist"
-    else
+    if ! grep -q "alias download='bash ~/.src/download.sh'" ~/.zshrc; then
         echo "alias download='bash ~/.src/download.sh'" >> ~/.zshrc
     fi
 fi
 
 if [ -f ~/.bashrc ]; then
-    if grep -q "alias download='bash ~/.src/download.sh'" ~/.bashrc; then
-        echo "alias download already exist"
-    else
+    if ! grep -q "alias download='bash ~/.src/download.sh'" ~/.bashrc; then
         echo "alias download='bash ~/.src/download.sh'" >> ~/.bashrc
     fi
 fi
 
 if [ -f ~/.zshrc ]; then
-    if grep -q "alias chrome='bash $install_dir/chrome/launch_chrome.sh'" ~/.zshrc; then
-        echo "alias chrome already exist"
-    else
+    if ! grep -q "alias chrome='bash $install_dir/chrome/launch_chrome.sh'" ~/.zshrc; then
         echo "alias chrome='bash $install_dir/chrome/launch_chrome.sh'" >> ~/.zshrc
     fi
 fi
 
 if [ -f ~/.bashrc ]; then
-    if grep -q "alias chrome='bash $install_dir/chrome/launch_chrome.sh'" ~/.bashrc; then
-        echo "alias chrome already exist"
-    else
+    if ! grep -q "alias chrome='bash $install_dir/chrome/launch_chrome.sh'" ~/.bashrc; then
         echo "alias chrome='bash $install_dir/chrome/launch_chrome.sh'" >> ~/.bashrc
     fi
 fi
 
 if [ -f ~/.zshrc ]; then
-    if grep -q "alias update='bash ~/.src/update.sh'" ~/.zshrc; then
-        echo "alias update already exist"
-    else
+    if ! grep -q "alias update='bash ~/.src/update.sh'" ~/.zshrc; then
         echo "alias update='bash ~/.src/update.sh'" >> ~/.zshrc
     fi
 fi
 
 if [ -f ~/.bashrc ]; then
-    if grep -q "alias update='bash ~/.src/update.sh'" ~/.bashrc; then
-        echo "alias update already exist"
-    else
+    if ! grep -q "alias update='bash ~/.src/update.sh'" ~/.bashrc; then
         echo "alias update='bash ~/.src/update.sh'" >> ~/.bashrc
     fi
 fi
